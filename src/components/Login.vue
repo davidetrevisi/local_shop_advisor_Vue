8c0daab1932083ddf1c80e17db023cf880895ef0
davidetrevisi <davide.trevisi19@gmail.com>
2022-05-27 15:12:46 +0200

    Separazione dei file script

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,38 +1,14 @@
 <script setup>
 import { ref, onMounted, watch } from "vue";
-import { loggedUser, setLoggedUser, clearLoggedUser } from "../states/users.js";
-
-const HOST = import.meta.env.VITE_API_HOST || `http://localhost:8080`;
-const API_URL = HOST + `/api/v1`;
+import { loggedUser, login, logout } from "../states/users.js";
 
 const emit = defineEmits(["login"]);
 const email = ref("");
 const password = ref("");
 
-function login() {
-  fetch(API_URL + "/authentications/login", {
-    method: "POST",
-    credentials: "include",
-    headers: { "Content-Type": "application/json", Accept: "application/json" },
-    body: JSON.stringify({ email: email.value, password: password.value }),
-  })
-    //.then((res) => console.log(res.data))
-    .then((res) => res.json())
-    .then(function (data) {
-      setLoggedUser(data);
-      emit("login", loggedUser);
-    })
-    .catch((error) => console.error(error)); // If there is any error you will catch them here
-}
-
-function logout() {
-  fetch(API_URL + "/authentications/logout", {
-    method: "GET",
-    credentials: "include",
-    headers: { "Content-Type": "application/json" },
-  })
-    .then(clearLoggedUser())
-    .catch((error) => console.error(error)); // If there is any error you will catch them here
+function loginButton() {
+  login(email.value, password.value);
+  emit("login", loggedUser);
 }
 </script>
 
@@ -40,7 +16,7 @@ function logout() {
   <form>
     <input v-model="email" placeholder="Email"/>
     <input v-model="password" placeholder="Password"/>
-    <button type="button" @click="login">LogIn</button>
+    <button type="button" @click="loginButton">LogIn</button>
     <button type="button" @click="logout" v-if="loggedUser.id !== undefined">LogOut</button>
   </form>
 </template>
