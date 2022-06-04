b646fb4713762a1347ff267f763efd0acc0540be
Stefano <stefano.pavan.22@gmail.com>
2022-06-04 18:13:16 +0200

    merge

--- src/components/CarrelloTable.vue
+++ src/components/CarrelloTable.vue
@@ -1,13 +1,8 @@
 <script setup>
 import { onBeforeMount } from 'vue'
 import { loggedUser } from '../states/users.js'
-<<<<<<< HEAD
 import { carrello, cercaCarrello, eliminaCarrello, svuotaCarrello} from '../states/carts.js'
 import { prodotto, fetchProdotto} from '../states/products.js'
-=======
-import { carrello, cercaCarrello, eliminaCarrello } from '../states/carts.js'
-import { prodotto, fetchProdotto } from '../states/products.js'
->>>>>>> 5d5398ae21df3ffed1825bb1087b4d29a3a6d477
 import { aggiungiOrdine } from '../states/orders.js'
 onBeforeMount(() => {
   fetchProdotto()
@@ -20,40 +15,21 @@ onBeforeMount(() => {
 
 
 <template>
-<<<<<<< HEAD
  <h3 v-if="loggedUser.id !== undefined"> Carrello di: {{loggedUser.email}} </h3>
     <ul>
       
         <li v-for="item in carrello.value.items" :key="item.value">
-            <h3>Prodotto: {{( prodotto.value.find(p=>p.id==item?.productId) || {name: 'Prodotto non presente nel catalogo, svuotare il carrello'} ).name}}</h3>
-            <h3>Prezzo: {{ item.price }} €</h3>
-            <h3>Quantitá: {{ item.quantity }}</h3>
-            <h3>Parziale: {{ item.total }} €</h3>
-            <button class="btn2" @click="eliminaCarrello(item.productId)">Elimina il prodotto dal carrello</button></li>
+            <h3 v-if="item.productId?.name !== undefined">Prodotto: {{item?.productId?.name}}</h3>
+            <h3 v-if="item.productId?.name === undefined" style="color: red" >Prodotto eliminato dal catalogo, svuotare il carrello</h3>
+            <h3 v-if="item.productId?.name !== undefined">Prezzo: {{ item.price }} €</h3>
+            <h3 v-if="item.productId?.name !== undefined">Quantitá: {{ item.quantity }}</h3>
+            <h3 v-if="item.productId?.name !== undefined">Parziale: {{ item.total }} €</h3>
+            <button v-if="item.productId?.name !== undefined" class="btn2" @click="eliminaCarrello(item.productId._id)">Elimina il prodotto dal carrello</button>
+            <button v-if="item.productId?.name === undefined" class="btn2" @click="svuotaCarrello(); $router.push('/catalogo')">Svuota il carrello</button></li>
             
     </ul>
 
     <h3>Totale: {{ carrello.value.subTotal }} €</h3>
-    <button class="btn2" @click="svuotaCarrello()">Svuota il Carrello</button>
-    <br/>
     <button class="btn2" :disabled="carrello.value.subTotal == 0"
     @click="aggiungiOrdine(); $router.push('/ordini')">Checkout</button>
-=======
-  <h3 v-if="loggedUser.id !== undefined"> Carrello di: {{ loggedUser.email }} </h3>
-  <ul>
-
-    <li v-for="item in carrello.value.items" :key="item.value">
-      <h3>Prodotto: {{ item.productId.name }}</h3>
-      <h3>Prezzo: {{ item.price }} €</h3>
-      <h3>Quantitá: {{ item.quantity }}</h3>
-      <h3>Parziale: {{ item.total }} €</h3>
-      <button class="btn2" @click="eliminaCarrello(item.productId._id)">Elimina il prodotto dal carrello</button>
-    </li>
-
-  </ul>
-
-  <h3>Totale: {{ carrello.value.subTotal }} €</h3>
-  <button class="btn2" :disabled="carrello.value.subTotal == 0"
-    @click="aggiungiOrdine(); $router.push('/ordini')">Checkout</button>
->>>>>>> 5d5398ae21df3ffed1825bb1087b4d29a3a6d477
 </template>
